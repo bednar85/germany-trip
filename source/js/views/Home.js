207cb0d216004971f5d3dc83bd2718d13b04f708
Ryan Bednar <Ryan_Bednar@epam.com>
2015-11-09 13:53:42 -0500

    General Update - fix path to JSON files, make relative - add links to Home View

--- source/js/views/Home.js
+++ source/js/views/Home.js
@@ -1,4 +1,6 @@
 var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
 
 
 
@@ -6,7 +8,20 @@ var React = require('react');
 var Home = React.createClass({
     render: function() {
         return (
-            <h2 className="">Home View</h2>
+            <div>
+                <h2 className="">Home View</h2>
+                <ul className="">
+                    <li className="col-sm-4 text-center">
+                        <Link to="destination" params={{destination: "berlin"}}>Berlin</Link>
+                    </li>
+                    <li className="col-sm-4 text-center">
+                        <Link to="destination" params={{destination: "munich"}}>Munich</Link>
+                    </li>
+                    <li className="col-sm-4 text-center">
+                        <Link to="destination" params={{destination: "vienna"}}>Vienna</Link>
+                    </li>
+                </ul>
+            </div>
         )
     }
 });
